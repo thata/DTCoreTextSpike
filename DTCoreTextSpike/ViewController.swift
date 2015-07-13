cd6641d81bba70ed91536b4b6d7eb1b1d518a428
takashi hatakeyama <takashi.hatakeyama@gmail.com>
2015-07-13 12:02:38 +0900

    Initial Commit

---
+++ DTCoreTextSpike/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DTCoreTextSpike
+//
+//  Created by Takashi Hatakeyama on 2015/07/13.
+//  Copyright (c) 2015年 esminc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
